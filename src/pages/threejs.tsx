c0f29558899412abc76c988618edbb6c38833f93
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2020-04-23 00:25:54 +0900

    add: sample component

--- src/pages/threejs.tsx
+++ src/pages/threejs.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Canvas } from 'react-three-fiber'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Box from '../components/organisms/box'
 
 const ThreeJs = () => (
   <Layout>
     <SEO title="three js practice" />
-    <h1>TBD</h1>
+    <h1>three js practice</h1>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
