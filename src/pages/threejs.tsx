fbe8abe2e25d85d03a0b99bb7cf16a8560d7beac
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2020-04-22 23:48:19 +0900

    add: empty page

---
+++ src/pages/threejs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThreeJs = () => (
+  <Layout>
+    <SEO title="three js practice" />
+    <h1>TBD</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ThreeJs
