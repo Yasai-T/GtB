763e23c801814f212d8180e921b5876241957c96
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2020-05-21 22:12:56 +0900

    init storybook

---
+++ stories/page-2.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Page2 from '../src/pages/page-2'
+import ThreeJs from '../src/pages/threejs'
+
+export default {
+  title: 'pages/page-2',
+}
+
+export const page2 = () => <Page2 />
+
+export const threeJs = () => <ThreeJs />
